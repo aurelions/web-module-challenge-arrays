4b7c71dd221746e339e26d354b94196299557aab
Trevor Mandina <trevormandina@Trevors-MBP.hitronhub.home>
2021-01-05 00:49:10 -0600

    Finished Arrays Project

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const originalFlavors = [
     "Vanilla Burnt Almond"
 ]
 
+
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: Copy the Array! 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 We have an array called originalFlavors with 31 flavors (see above).  In these tasks, we will be reading and writing data to this array.  
 With all of these changes going on, we don't want to lose track of the actual, original 31 flavors.  So we need to copy the original array!
@@ -45,9 +46,10 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy(arr) {
+    return [...arr]
+    }
+    console.log(copy(originalFlavors));  
 
 
 
@@ -64,9 +66,19 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(originalFlavors)
+{
+  if (originalFlavors.length = 31)
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
 }
+
+console.log(is31Flavors(originalFlavors));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,9 +93,14 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(originalFlavors)
+{
+originalFlavors.unshift("Rainbow Sherbert!");
+  return originalFlavors;
 }
+
+console.log(addFlavor(originalFlavors));
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -97,9 +114,12 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(originalFlavors)
+{
+    originalFlavors.pop();
+  return originalFlavors;
 }
+console.log(removeLastFlavor(originalFlavors));
 
 
 
@@ -114,9 +134,12 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(originalFlavors)
+{
+   originalFlavors.splice(1, 0, "Black Walnut");
+  return originalFlavors;
 }
+console.log(getFlavorByIndex(originalFlavors));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -134,9 +157,12 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(originalFlavors)
+{
+   originalFlavors.splice(29, 1)
+  return originalFlavors;
 }
+console.log(removeFlavorByName(originalFlavors));
 
 
 
@@ -160,9 +186,17 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(originalFlavors)
+{
+  if (originalFlavors.includes("Chocolate"))
+    {
+      return newArray = ["Chocolate", "Chocolate Chip", "Chocolate Fudge", "Chocolate Mint" , "Chocolate Ribbon"];
+    }
+  else{
+    return false;
+  }
 }
+console.log(filterByWord(originalFlavors));
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
@@ -299,4 +333,3 @@ export default{
     getAverageWordLength,
     getRandomFlavors
 }
-
